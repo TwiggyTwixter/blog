f6a4cc01a1320ea0edbfe4b841468bfeac829a53
TwiggyTwixter <twiggytwixt@gmail.com>
2019-08-29 20:51:40 -0600

    added public directory path to express and created a link to about me page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars')
+const path = require('path')
 
 const app = express();
 const port = 3000;
@@ -8,10 +9,15 @@ const port = 3000;
 app.engine('handlebars', handlebars())
 app.set("view engine", 'handlebars')
 
+app.use(express.static(path.join(__dirname, '/public')))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/about', (req, res) => {
+    res.send("Hello")
+})
 app.listen(port, () => {
     console.log(`Listening to port ${port}`)
 })
